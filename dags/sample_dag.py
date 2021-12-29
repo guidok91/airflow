dee2480ef84025ef35264edc193ddf3ef9844089
GK <guidokosloff@gmail.com>
2021-12-29 13:04:18 -0300

    Readd sample DAG

---
+++ dags/sample_dag.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="dummy",
+    default_args={
+        "owner": "Guido Kosloff",
+        "depends_on_past": False,
+        "email_on_failure": False,
+        "email_on_retry": False,
+    },
+    start_date=datetime(2021, 1, 1, 0, 0),
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+
+    start = DummyOperator(
+        task_id="start"
+    )
+
+    end = DummyOperator(
+        task_id="end"
+    )
+
+    start >> end
